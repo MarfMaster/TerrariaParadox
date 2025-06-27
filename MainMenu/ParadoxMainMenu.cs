853fe3b4c542b95a7ea19f8fc4857d131a342b51
Fusion243 <nathan.flety3@gmail.com>
2025-06-27 20:39:48 +0100

    Create ParadoxMainMenu.cs

---
+++ MainMenu/ParadoxMainMenu.cs
@@ -0,0 +1,13 @@
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+
+namespace TerrariaParadox.MainMenu
+{
+    public class ParadoxMainMenu : ModMenu
+    {
+        public override string DisplayName => "TERRARIA: PARADOX";
+        
+        public override Asset<Texture2D> Logo => ModContent.Request<Texture2D>("TerrariaParadox/MainMenu/MenuLogo");
+    }
+}
